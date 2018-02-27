7933e774883f168377c5f6450ddd1cded83542d2
walterspieler <matthieu@walterspieler.com>
2018-02-27 20:03:29 +0100

    Added Subscribers, Actions, Reducers and Store

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import * as fromStore from './store';
+
 import { renderTodos } from './utils';
 
 const input = document.querySelector('input') as HTMLInputElement;
@@ -5,23 +7,40 @@ const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
 
+const reducers = {
+  todos: fromStore.reducer
+};
+
+const store = new fromStore.Store(reducers);
+
+console.log(store.value);
+
 button.addEventListener(
   'click',
   () => {
     if (!input.value.trim()) return;
 
-    const payload = { label: input.value, complete: false };
+    const todo = { label: input.value, complete: false };
 
-    console.log(payload);
+    store.dispatch(new fromStore.AddTodo(todo));
 
     input.value = '';
   },
   false
 );
 
+const unsubscribe = store.subscribe(state => {
+  renderTodos(state.todos.data);
+});
+
 todoList.addEventListener('click', function(event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === 'button') {
-    console.log(target);
+    const todo = JSON.parse(target.getAttribute('data-todo') as any);
+    store.dispatch(new fromStore.RemoveTodo(todo));
   }
 });
+
+destroy.addEventListener('click', unsubscribe, false);
+
+store.subscribe(state => console.log('STATE:::', state));
